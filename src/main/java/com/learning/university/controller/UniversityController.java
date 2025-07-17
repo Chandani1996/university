eb5781562a0ce5bff28dafb0ba9ec88180b2b2d4
Chandani <naikchandani17@gmail.com>
2025-07-17 17:41:01 +0530

    Added exception handling for the controllers

--- src/main/java/com/learning/university/controller/UniversityController.java
+++ src/main/java/com/learning/university/controller/UniversityController.java
@@ -1,9 +1,10 @@
 package com.learning.university.controller;
 
-import com.learning.university.model.StudentDTO;
 import com.learning.university.model.UniversityDTO;
 import com.learning.university.service.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +17,34 @@ public class UniversityController {
     private UniversityService universityService;
 
     @PostMapping("/save")
-    public UniversityDTO saveUniversityDetails(@RequestBody UniversityDTO universityDTO) {
-        return universityService.saveUniversityDetails(universityDTO);
+    public ResponseEntity<Object> saveUniversityDetails(@RequestBody UniversityDTO universityDTO) {
+        try {
+            UniversityDTO universityDetails = universityService.saveUniversityDetails(universityDTO);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(universityDetails);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
     }
 
     @GetMapping("/university/{id}")
-    public List<String> displayUniversityStudentDetails(@PathVariable Integer id) {
-        return universityService.fetchUniversityStudentnames(id);
+    public ResponseEntity<Object> displayUniversityStudentDetails(@PathVariable Integer id) {
+        try {
+            List<String> universityNameList = universityService.fetchUniversityStudentnames(id);
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(universityNameList);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
     }
-
 }
